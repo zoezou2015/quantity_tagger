6655cd9f0da3eae8e83fdf889ee55bd65ce6557b
yanyan <zoe.yyzou@gmail.com>
2019-05-25 18:55:32 +0800

    update readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# The StatNLP Semantic Parser
+
+Code for the: Quantity Tagger: A Latent-Variable Sequence Labeling Approach to Solving Addition-Subtraction Word Problems
+
+## Requirements
+Java 1.8
+Standford CoreNLP toolkits
+
+
+A jar file in provided in `bin/`. One easy way to re-compile the code is to create a new project in Eclipse and export a runnable JAR file.
+
+## Reproducing the experimental results
+
+The following script will run the `quantity_tagger` and its variants over two datasets, respectively.
+
+```
+bash run.sh
+```
+
+
+
---
+++ run.sh
@@ -0,0 +1,23 @@
+# AddSub
+
+#QT
+java  -Djava.library.path=/usr/local/lib -cp bin/math_addsub.jar org.nlp.example.MathAddSubMain -lang en > qt_as
+#QT(S)
+java  -Djava.library.path=/usr/local/lib -cp bin/math_addsub.jar org.nlp.example.SemiMathAddSubMain  -lang en > sqt_as
+#QT(R)
+java  -Djava.library.path=/usr/local/lib -cp bin/math_addsub.jar org.nlp.example.RelaxMathAddSubMain  -lang en > rqt_as
+#QT(FIX)
+java -Djava.library.path=/usr/local/lib -cp bin/math_addsub.jar org.nlp.example.FixMathAddSubMain  -lang en > fqt_as
+
+
+# Math23K_AS
+
+#QT
+java -Djava.library.path=/usr/local/lib -cp bin/math_addsub.jar org.nlp.example.VariantMathAddSubMain  -lang zh > qt_ma
+#QT(S)
+java -Djava.library.path=/usr/local/lib -cp bin/math_addsub.jar org.nlp.example.SemiMathAddSubMain -lang zh > sqt_ma
+#QT(R)
+java -Djava.library.path=/usr/local/lib -cp bin/math_addsub.jar org.nlp.example.RelaxMathAddSubMain -lang zh > rqt_ma
+#QT(FIX)
+java -Djava.library.path=/usr/local/lib -cp bin/math_addsub.jar org.nlp.example.FixMathAddSubMain -lang zh > fqt_ma
+
